6ebf506a13b580dcd0860ed76dbbc3653ff50b15
Sami Munir <smunir01@outlook.com>
2024-02-06 04:38:53 -0500

    adding 1. Two Sum - C#

---
+++ 1. Two Sum/two-sum/Program.cs
@@ -0,0 +1,13 @@
+﻿/*
+    Sami Munir
+    February 6th, 2024
+    LeetCode Journey
+    1. Two Sum - C#
+*/
+
+public class Program {
+    public static void Main(string[] args) {
+        Console.WriteLine("1. Two Sum");
+        Console.WriteLine("----------\n");
+    }
+}
--- ReadMe.md
+++ ReadMe.md
@@ -1,2 +1,3 @@
 Sami Munir | Rutgers University | smunir01@outlook.com
 # LeetCode Journey
+## 1. Two Sum
