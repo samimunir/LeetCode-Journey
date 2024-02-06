3c3eaeb6f6fd5a4c2fd22c1b22cc354644a8e8bd
Sami Munir <smunir01@outlook.com>
2024-02-06 05:07:07 -0500

    setting up main function & solution class

--- 1. Two Sum/two-sum/Program.cs
+++ 1. Two Sum/two-sum/Program.cs
@@ -9,5 +9,9 @@ public class Program {
     public static void Main(string[] args) {
         Console.WriteLine("1. Two Sum");
         Console.WriteLine("----------\n");
+
+        Console.WriteLine("Solution -->");
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
     }
 }
---
+++ 1. Two Sum/two-sum/Solution.cs
@@ -0,0 +1,14 @@
+public class Solution {
+    private int[] nums;
+    private int target;
+
+    public Solution(int[] nums, int target) {
+        this.nums = nums;
+        this.target = target;
+    }
+
+    public int[] TwoSum(int[] nums, int target) {
+        Dictionary<int, int> nums_map = new Dictionary<int, int>();
+        return null;
+    }
+}
